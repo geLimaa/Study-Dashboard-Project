588ddc2f6712460023ce6da499cac8e9cb48a001
Limaa0907 <geancarloslima0907@gmail.com>
2026-05-04 15:00:53 -0300

    Main dashboard done

---
+++ scripts/tasks.js
@@ -0,0 +1,10 @@
+/*
+const goToTasks = document.querySelector(".go-to-tasks-section");
+
+goToTasks.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  window.location.href = "./tasks.html";
+});
+*/
+
